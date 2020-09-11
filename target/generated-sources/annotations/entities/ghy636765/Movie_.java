5c4d8a8d1d825aed7b752f9f6909c032f3084e0b
KingBendico <cph-bc105@cphbusiness.dk>
2020-09-11 18:31:40 +0200

    some edits

--- target/generated-sources/annotations/entities/ghy636765/Movie_.java
+++ target/generated-sources/annotations/entities/ghy636765/Movie_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-11T18:15:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-11T18:26:46")
 @StaticMetamodel(Movie.class)
 public class Movie_ { 
 
